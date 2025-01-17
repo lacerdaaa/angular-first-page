21180ec43097e7bfb78ac4a7b407bac024ad8f42
lacerda <edulacerdaaa@gmail.com>
2025-01-17 11:28:37 -0300

    - adiciona formulário de inscrição com validação e estilos

--- src/app/components/news-form/news-form.component.ts
+++ src/app/components/news-form/news-form.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormGroup, ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'news-form',
   standalone: true,
-  imports: [],
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
   templateUrl: './news-form.component.html',
   styleUrl: './news-form.component.scss'
 })
 export class NewsFormComponent {
+  newLetterForm: FormGroup;
 
+  constructor() {
+    this.newLetterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    })
+  }
+
+  submit() {
+    console.log(this.newLetterForm.value);
+  }
 }
