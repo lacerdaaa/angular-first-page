dae006847fda216aab2dd8bc804cfa0c85611bf6
lacerda <edulacerdaaa@gmail.com>
2025-01-16 23:12:25 -0300

    - adiciona componente de botão primário com variantes e estilos

--- src/app/components/btn-primary/btn-primary.component.ts
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+type BtnVariant = "primary" | "secondary";
+
 
 @Component({
-  selector: 'app-btn-primary',
+  selector: 'btn-primary',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './btn-primary.component.html',
   styleUrl: './btn-primary.component.scss'
 })
 export class BtnPrimaryComponent {
+  @Input() variant: BtnVariant = "primary";
+  @Input("btnText") btnText: string = "";
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() boolean = false;
+  @Output("submit") onSubmit = new EventEmitter();
 
+  submit() {
+    this.onSubmit.emit();
+  }
 }
---
+++ src/app/components/news-form/news-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './news-form.component.html',
+  styleUrl: './news-form.component.scss'
+})
+export class NewsFormComponent {
+
+}
