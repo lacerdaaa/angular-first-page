f968abe4d2a2ac0466189bc27c61555bfb32257d
lacerda <edulacerdaaa@gmail.com>
2025-01-16 23:57:57 -0300

    - adiciona formulário de notícias com campos de entrada e botão primário

--- src/app/components/news-form/news-form.component.ts
+++ src/app/components/news-form/news-form.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 
 @Component({
-  selector: 'app-news-form',
+  selector: 'news-form',
   standalone: true,
   imports: [],
   templateUrl: './news-form.component.html',
