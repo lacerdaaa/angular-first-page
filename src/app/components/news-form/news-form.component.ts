f43e13755a1695d2d3306513e9f2be126fd2ba8f
lacerda <edulacerdaaa@gmail.com>
2025-01-17 14:10:31 -0300

    - adiciona serviço de newsletter e integra ao formulário de notícias

--- src/app/components/news-form/news-form.component.ts
+++ src/app/components/news-form/news-form.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { FormGroup, ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
 
 
 @Component({
   selector: 'news-form',
   standalone: true,
   imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers: [NewsletterService],
   templateUrl: './news-form.component.html',
   styleUrl: './news-form.component.scss'
 })
 export class NewsFormComponent {
   newLetterForm: FormGroup;
 
-  constructor() {
+  constructor( private service: NewsletterService ) {
     this.newLetterForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -21,6 +23,12 @@ export class NewsFormComponent {
   }
 
   submit() {
-    console.log(this.newLetterForm.value);
+    if( this.newLetterForm.valid) {
+      this.service.sendData(this.newLetterForm.value.name, this.newLetterForm.value.email).subscribe({
+        next: () => {
+          this.newLetterForm.reset();
+        }
+      })
+    }
   }
 }
